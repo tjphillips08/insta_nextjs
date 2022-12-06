e54534563bdb93e2149c72d0aff2650f763f26ef
tjphillips08 <tjphillips08@icloud.com>
2022-12-06 10:32:22 -0600

    user state passed between components

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import "@fortawesome/fontawesome-free/js/fontawesome";
+import "@fortawesome/fontawesome-free/js/solid";
+import "@fortawesome/fontawesome-free/js/regular";
+import "@fortawesome/fontawesome-free/js/brands";
+import { UserProvider } from "@auth0/nextjs-auth0/client";
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <UserProvider>
+      <Component {...pageProps} />
+    </UserProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
