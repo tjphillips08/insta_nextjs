b18370c6fcddfa7093abe4254c9cddb7bdf88a98
tjphillips08 <tjphillips08@icloud.com>
2022-12-06 10:40:24 -0600

    next config set up properly

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  swcMinify: true,
+  images: {
+    domains: [
+      "catstagram.lofty.codes",
+      "storage.googleapis.com",
+      "lh3.googleusercontent.com",
+    ],
+  },
+};
 
 module.exports = nextConfig
