673f97bd7cd4d3baf5c99cec6fdf4a37bd85f1f6
tjphillips08 <tjphillips08@icloud.com>
2022-12-07 11:08:45 -0600

    added styling to nav bar

--- components/NavigationBar.js
+++ components/NavigationBar.js
@@ -1,67 +1,72 @@
 import React from "react";
 import Link from "next/link";
-import Image from "next/image";
 import { faCat } from "@fortawesome/free-solid-svg-icons";
+import { faCameraRetro } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Login from "./Login";
+import { useUser } from "@auth0/nextjs-auth0/client";
+import { motion } from "framer-motion";
 
-import { useRouter } from "next/router";
+const NavigationBar = (props) => {
+  const { user, error, isLoading } = useUser();
 
-const NavigationBar = () => {
-  const router = useRouter();
-  const isCurrentPath = (path) => router.pathname === path;
   return (
-    <nav className="flex justify-between bg-teal-500 p-6">
+    <nav className="flex lg:p-6 justify-between md:justify-around bg-teal-500 p-4 sticky top-0 shadow-lg">
       <div className="flex items-center text-white mr-6">
         <Link className="font-semibold text-xl tracking-tight ml-2" href="/">
           InstaCats
         </Link>
       </div>
       <div className="flex items-center">
-        <FontAwesomeIcon
-          className="text-slate-600 text-6xl pr-2 "
-          icon={faCat}
-        />
+        <Link className="font-semibold text-xl tracking-tight ml-2" href="/">
+          <motion.div
+            animate={{ rotate: [-5, 5, -5, 5, 0], scale: [1, 1.05, 1] }}
+            transition={{ duration: 0.5, ease: "easeInOut", repeatDelay: 1 }}
+            whileHover={{ y: -5, scale: 1.1 }}
+          >
+            <FontAwesomeIcon
+              className="text-slate-600 text-6xl pr-4 pl-4 "
+              icon={faCat}
+            />
+          </motion.div>
+        </Link>
+        {user && (
+          <div className="text-sm flex items-center">
+            <Link
+              className="visible sm:text-xs text-xl sm:invisible"
+              href="/Upload"
+            >
+              <motion.div
+                animate={{ rotate: [-5, 5, -5, 5, 0], scale: [1, 1.05, 1] }}
+                transition={{
+                  duration: 0.5,
+                  ease: "easeInOut",
+                  repeatDelay: 1,
+                }}
+                whileHover={{ y: -5, scale: 1.1 }}
+              >
+                <FontAwesomeIcon
+                  className="text-slate-600 rotate-45  visible sm:invisible"
+                  icon={faCameraRetro}
+                />
+              </motion.div>
+            </Link>
+          </div>
+        )}
       </div>
 
-      <div className="flex lg:flex lg:items-center lg:w-auto">
-        <div className="text-sm lg:flex-grow">
-          <Link
-            className={`block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4 ${
-              isCurrentPath("/upload") ? "text-white font-bold" : ""
-            }`}
-            href="/upload"
-          >
-            Upload Photo
-          </Link>
-          <Link
-            className={`block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4 ${
-              isCurrentPath("/login") ? "text-white font-bold" : ""
-            }`}
-            href="/login"
-          >
-            Login
-          </Link>
-          <Link
-            className={`block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white ${
-              isCurrentPath("/register") ? "text-white font-bold" : ""
-            }`}
-            href="/register"
-          >
-            Register
-          </Link>
-        </div>
-        <div className="lg:hidden flex items-center">
-          <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
-            <svg
-              className="fill-current h-3 w-3"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
+      <div className="flex items-center">
+        {user && (
+          <div className="text-sm flex items-center">
+            <Link
+              className="invisible md:pr-6 text-xs sm:text-base sm:visible"
+              href="/Upload"
             >
-              <title>Menu</title>
-              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-            </svg>
-          </button>
-        </div>
+              Upload Photo
+            </Link>
+          </div>
+        )}
+        <Login></Login>
       </div>
     </nav>
   );
