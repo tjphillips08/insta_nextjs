ce6b65afc4efa3e382e1940e7a5b14d8c0091e2d
tjphillips08 <tjphillips08@icloud.com>
2022-12-07 11:06:47 -0600

    useUser state implemented and added styling to login

--- components/Login.js
+++ components/Login.js
@@ -3,6 +3,7 @@ import { useUser } from "@auth0/nextjs-auth0/client";
 import Link from "next/link";
 import { Menu } from "@headlessui/react";
 import Image from "next/image";
+import { motion } from "framer-motion";
 const Login = () => {
   const { user, isLoading } = useUser();
   return (
@@ -16,23 +17,41 @@ const Login = () => {
         <div>
           <div>
             <Menu>
-              <Menu.Button className="text-slate-700 pl-6 relative">
-                <Image
-                  src={user.picture}
-                  alt="Profile"
-                  className="nav-user-profile rounded-full shadow-lg"
-                  width="50"
-                  height="50"
-                  decode="async"
-                  data-testid="navbar-picture-desktop"
-                />
+              <Menu.Button className="text-slate-700 relative">
+                <motion.div
+                  initial={{ scale: 1 }}
+                  whileHover={{
+                    scale: 1.2,
+                    transition: { duration: 0.5 },
+                    rotate: [0, 0, 8, -8, 0, 0],
+                    transition: {
+                      duration: 0.5,
+                      times: [0, 0.2, 0.5, 0.8, 1],
+                      ease: "easeInOut",
+                    },
+                  }}
+                  whileTap={{ scale: 0.9 }}
+                  className='h-10 w-10 relative'
+                >
+                  <Image
+                    src={user.picture}
+                    alt="Profile"
+                    className="md:scale-125 nav-user-profile rounded-full shadow-lg"
+                    layout="fill" // required
+                    objectFit="cover"
+                    decode="async"
+                    data-testid="navbar-picture-desktop"
+                  />{" "}
+                </motion.div>
               </Menu.Button>
-              <Menu.Items className="text-neutral-200 rounded-lg flex flex-col bg-slate-900 absolute">
-                <Menu.Item>{({ active }) => <p> {user.name}</p>}</Menu.Item>
-                <Menu.Item>
+              <Menu.Items className="text-neutral-100 rounded-lg flex flex-col bg-slate-900 absolute">
+                <Menu.Item className="p-2">
+                  {({ active }) => <p> {user.name}</p>}
+                </Menu.Item>
+                <Menu.Item className="p-2">
                   {({ active }) => (
                     <Link
-                      href="/profile"
+                      href="/Profile"
                       icon="user"
                       testId="navbar-profile-desktop"
                     >
@@ -40,7 +59,7 @@ const Login = () => {
                     </Link>
                   )}
                 </Menu.Item>
-                <Menu.Item disabled>
+                <Menu.Item className="p-2" disabled>
                   <Link
                     href="/api/auth/logout"
                     icon="power-off"
